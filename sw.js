b9032c7fa036bcda3258ab7e75351fbb7f606a59
Jack Heyworth <jack.heyworth@byrom.plc.uk>
2026-04-17 17:21:24 +0100

    fix v0.3

--- sw.js
+++ sw.js
@@ -1,27 +1,25 @@
-const cacheName = 'cycle-app-v2';
-const assets = [
-  '/CycleApp/',
-  '/CycleApp/index.html',
-  '/CycleApp/style.css',
-  '/CycleApp/app.js',
-  '/CycleApp/data.js',
-  '/CycleApp/manifest.json',
-  '/CycleApp/icon-192.png',
-  '/CycleApp/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/CycleApp/',
+        '/CycleApp/index.html',
+        '/CycleApp/style.css',
+        '/CycleApp/app.js',
+        '/CycleApp/data.js',
+        '/CycleApp/manifest.json',
+        '/CycleApp/icon-192.png',
+        '/CycleApp/icon-512.png'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// THIS PART IS CRITICAL FOR THE INSTALL BUTTON
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
